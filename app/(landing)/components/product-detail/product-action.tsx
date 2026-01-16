d96555858d01ac20e831f3245194aa407c706e60
Ehaqq <a.adamezra@gmail.com>
2026-01-16 16:02:55 +0700

    continue slicing live session 2

---
+++ app/(landing)/components/product-detail/product-action.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { FiArrowRight,  FiChevronDown, FiChevronUp, FiShoppingBag } from "react-icons/fi";
+import Button from "../ui/button";
+import React, { use } from "react";
+import { useRouter } from "next/navigation";
+
+const ProductAction = () => {
+    const {push} = useRouter();
+
+    const [qty, setQty] = React.useState(1);
+
+    const checkout = () => {
+
+    };
+
+  return (
+    <div className="flex gap-5">
+        <div className="border border-gray-500 inline-flex w-fit min-w-20.5">
+            <div className="aspect-square text-xl font-medium border-r border-gray-500 flex justify-center items-center">
+                <span>{qty}</span>
+            </div>
+            <div className="flex flex-col">
+                <div className="border-b border-gray-500 cursor-pointer h-1/2 aspect-square flex justify-center items-center"
+                    onClick={() => setQty(qty + 1)}
+                >
+                    <FiChevronUp/>
+                </div>
+                <div className="cursor-pointer h-1/2 aspect-square flex justify-center items-center"
+                    onClick={() => setQty(qty > 1 ? qty - 1 : qty)}
+                >
+                    <FiChevronDown/>
+                </div>
+            </div>
+        </div>
+        <Button className="px-20 w-full"><FiShoppingBag size={24}/>Add To Cart</Button>
+        <Button variant="dark"className="px-20 w-full" onClick={() => push("/checkout")}>Checkout Now <FiArrowRight size={24}/></Button>
+    </div>
+  )
+}
+
+export default ProductAction;
